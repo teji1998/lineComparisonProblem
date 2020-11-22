b698b4e2a107fd3c24650972157bdaaa324d92fc
“teji1998” <tejasvinirpk@gmail.com>
2020-11-22 19:51:38 +0530

    Added:Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lineComparisonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation!");
+        }
+    }
+}
