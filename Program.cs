1a9a8d97a79c3e6bb49e066d0c3acc6ecd4e5165
“teji1998” <tejasvinirpk@gmail.com>
2020-11-22 20:09:57 +0530

    Modified:Added comments

--- Program.cs
+++ Program.cs
@@ -6,17 +6,27 @@ namespace lineComparisonProblem
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to Line Comparison Computation!");
             int x1, x2;
             int y1, y2;
+
+            // Taking the coordinates for first point of line
             Console.WriteLine("Enter the values of x1 and y1 coordinates of the line");
+           
+            //Convert.ToInt32 is used to convert a value into 32bit specified integer
             x1 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
+           
+            // Taking the coordinates for second point of line
             Console.WriteLine("Enter the values of x2 and y2 coordinates of the line");
             x2 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
+           
+            //Calculating the length of line
             double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length of the line is :" + length);
+            Console.WriteLine("Length of the line is : " + length);
+
         }
     }
 }
