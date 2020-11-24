912dc633344eac8c19c41a7231c82083b6b87fbf
“teji1998” <tejasvinirpk@gmail.com>
2020-11-24 05:42:42 +0530

    Refactored:Used methods to compare the length of the two lines

--- Program.cs
+++ Program.cs
@@ -4,53 +4,29 @@ namespace lineComparisonProblem
 {
     class Program
     {
-        static void Main(string[] args)
+        //Method to find the distance between the end points
+        public static double EndPointsDistance()
         {
+            Console.WriteLine("Enter the value of first point and second point");
+            double pointOne = Convert.ToDouble(Console.ReadLine());
+            double pointTwo = Convert.ToDouble(Console.ReadLine());
+            double distanceBetweenPoints = Math.Pow((pointTwo - pointOne), 2);
+            return distanceBetweenPoints;
+        }
 
-            //Welcome message 
-            Console.WriteLine("Welcome to Line Comparison Computation!");
+        //Method to find the length of the line
+        public static double LengthOfLine()
+        {
+           double xCoordinate = EndPointsDistance();
+           double yCoordinate = EndPointsDistance();
+           double lengthOfLine = Math.Sqrt(xCoordinate + yCoordinate);
+            return lengthOfLine;
+        }
 
-            //Taking four sets of variables are used to check equality of the two lines
-            int x1, x2, x3, x4, y1, y2, y3, y4;
-            
-            // Taking the coordinates for first point of line one
-            Console.WriteLine("Enter the values of x1 and y1 coordinates of the line one");
-           
-            //Convert.ToInt32 is used to convert a value into 32bit specified integer
-            x1 = Convert.ToInt32(Console.ReadLine());
-            y1 = Convert.ToInt32(Console.ReadLine());
-           
-            // Taking the coordinates for second point of line one
-            Console.WriteLine("Enter the values of x2 and y2 coordinates of the line one");
-
-            x2 = Convert.ToInt32(Console.ReadLine());
-            y2 = Convert.ToInt32(Console.ReadLine());
-
-            // Taking the coordinates for first point of line two
-            Console.WriteLine("Enter the values of x3 and y3 coordinates of the line two");
-
-            x3 = Convert.ToInt32(Console.ReadLine());
-            y3 = Convert.ToInt32(Console.ReadLine());
-
-            // Taking the coordinates for second point of line two
-            Console.WriteLine("Enter the values of x4 and y4 coordinates of the line two");
-
-            x4 = Convert.ToInt32(Console.ReadLine());
-            y4 = Convert.ToInt32(Console.ReadLine());
-
-            //Calculating the length of line one
-            double lengthOfLineOne = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length of the line one is : " + lengthOfLineOne);
-
-            //Calculating the length of line two
-            double lengthOfLineTwo = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            Console.WriteLine("Length of the line two is : " + lengthOfLineTwo);
-
-            //CompareTo method is used to compare instance of length of line by returning value of instance
-            //to be greater than,lesser than or equal
+        //Method to compare the lengths of the two lines
+        public static int ComparingLengthOfLines(double lengthOfLineOne, double lengthOfLineTwo) 
+        {
             int differenceInLength = lengthOfLineOne.CompareTo(lengthOfLineTwo);
-
-            //Using if loop to check if the two lines are equal ,smaller than or greater than in length
             if (differenceInLength == 0)
                 Console.WriteLine("Length of the two lines are equal.");
             else if (differenceInLength > 0)
@@ -58,6 +34,25 @@ namespace lineComparisonProblem
             else
                 Console.WriteLine("Length of line two is greater than length of line one");
 
+            return differenceInLength;
+           
+        }
+
+        static void Main(string[] args)
+        {
+
+            //Welcome message 
+            Console.WriteLine("Welcome to Line Comparison Computation!");   
+            
+            //Calling the method to calculate the length of the line
+            double lengthOfLineOne = LengthOfLine();
+            Console.WriteLine("The length of line one : " + lengthOfLineOne);
+            double lengthOfLineTwo = LengthOfLine();
+            Console.WriteLine("The length of line two : " + lengthOfLineTwo);
+
+            //Calling the method to compare the length of the lines
+            int comparingLines = ComparingLengthOfLines(lengthOfLineOne, lengthOfLineTwo);
+           
         }
 
     }
